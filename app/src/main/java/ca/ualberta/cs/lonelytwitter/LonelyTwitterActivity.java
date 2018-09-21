56fadb1bf056e6059053d2c222c783b8be739e19
derraj <mahinay@ualberta.ca>
2018-09-20 19:52:17 -0600

    message

--- app/src/main/java/ca/ualberta/cs/lonelytwitter/LonelyTwitterActivity.java
+++ app/src/main/java/ca/ualberta/cs/lonelytwitter/LonelyTwitterActivity.java
@@ -37,10 +37,15 @@ public class LonelyTwitterActivity extends Activity {
 		saveButton.setOnClickListener(new View.OnClickListener() {
 
 			public void onClick(View v) {
-				setResult(RESULT_OK);
 				String text = bodyText.getText().toString();
+
+                ImportantTweet importantTweet = new ImportantTweet();
+                try {
+                    importantTweet.setMessage(text);
+                }catch(TooLongTweetException e){
+
+                }
 				saveInFile(text, new Date(System.currentTimeMillis()));
-				finish();
 
 			}
 		});
